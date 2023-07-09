ef2895a29aac93b7f7ff26971eefe81ffa22be1d
Jay035 <jaydencolins08@gmail.com>
2023-07-09 15:43:57 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,13 @@ import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import HowItWorks from "@/components/HowItWorks";
 import MeetOurDoctors from "@/components/MeetOurDoctors";
-import { Navbar } from "@/components/Navbar";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import Testimonials from "@/components/Testimonials";
 import Why from "@/components/Why";
-import { AuthProvider } from "@/context/Auth";
 
 export default function Home() {
   return (
     // dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert/
-    <AuthProvider>
-      <Navbar />
       <main className="mt-[4.56rem] mb-[3.375rem]">
         <HeroSection />
         <HowItWorks />
@@ -24,6 +20,5 @@ export default function Home() {
         <NewsLetter />
         <Footer />
       </main>
-    </AuthProvider>
   );
 }
