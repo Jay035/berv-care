ff32c02df356fce9e1a0943fb79c83602dec2dcf
Jay035 <jaydencolins08@gmail.com>
2023-09-13 18:32:39 +0100

    fix

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -10,24 +10,24 @@ export default function Transition() {
     let ctx = gsap.context(() => {
     gsap.to(".first", {
       delay: .2,
-      right: "-100%",
+      top: "-100%",
       ease: Power3.easeInOut,
     });
-    // gsap.to(".second", {
-    //   delay: .4,
-    //   top: "-100%",
-    //   ease: Power3.easeInOut,
-    // });
-    // gsap.to(".third", {
-    //   delay: .6,
-    //   top: "-100%",
-    //   ease: Power3.easeInOut,
-    // });
-    // gsap.to(".fourth", {
-    //   delay: .8,
-    //   top: "-100%",
-    //   ease: Power3.easeInOut,
-    // });
+    gsap.to(".second", {
+      delay: .4,
+      top: "-100%",
+      ease: Power3.easeInOut,
+    });
+    gsap.to(".third", {
+      delay: .6,
+      top: "-100%",
+      ease: Power3.easeInOut,
+    });
+    gsap.to(".fourth", {
+      delay: .8,
+      top: "-100%",
+      ease: Power3.easeInOut,
+    });
     // gsap.to(".fifth", {
     //   delay: 1,
     //   top: "-100%",
@@ -38,11 +38,11 @@ export default function Transition() {
   }, []);
   return (
     <div ref={trans} className="overflow-x-hidden w-fit">
-      <div className="transition first absolute right-0 top-0 w-full h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div>
-      {/* <div className="transition second absolute left-[20%] top-0 w-full h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div> */}
-      {/* <div className="transition third absolute left-[40%] top-0 w-full h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div>
-      <div className="transition fourth absolute left-[60%] top-0 w-full h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div> */}
-      {/* <div className="transition fifth absolute left-[80%] top-0 w-full h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div> */}
+      <div className="transition first absolute left-0 top-0 w-1/4 h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div>
+      <div className="transition second absolute left-[20%] top-0 w-1/4 h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div>
+      <div className="transition third absolute left-[40%] top-0 w-1/4 h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div>
+      <div className="transition fourth absolute left-[60%] top-0 w-1/4 h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div>
+      {/* <div className="transition fifth absolute left-[80%] top-0 w-1/4 h-full z-[10000000000000000000000000000000000000] bg-[#14532dc9]"></div> */}
     </div>
   );
 }
