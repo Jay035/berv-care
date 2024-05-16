2a04c38f253b3cf266a07d6f2f67794a024360d7
Jay035 <jaydencolins08@gmail.com>
2024-05-16 23:29:58 +0100

    update

--- app/postBlog/Form.tsx
+++ app/postBlog/Form.tsx
@@ -49,7 +49,7 @@ export default function Form({
       </section>
       <section className="col-span-1 ">
         <h1 className="text-lg font-medium mb-3">Preview</h1>
-        <div className="border rounded-lg p-4 prose">
+        <div className="border rounded-lg p-4 prose prose-a:text-[#DD2D4A]">
           <h1 className="mb-2 font-bold">{title && title}</h1>
           <div className="">
             {markdown && (
