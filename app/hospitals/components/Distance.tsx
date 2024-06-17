7e1177037def8c931ac590afb4b07368475266f3
Jay035 <jaydencolins08@gmail.com>
2024-06-17 02:44:52 +0100

    update

--- app/hospitals/components/Distance.tsx
+++ app/hospitals/components/Distance.tsx
@@ -1,11 +1,17 @@
-
 type Props = {
-    leg: google.maps.DirectionsLeg
-}
+  leg: google.maps.DirectionsLeg;
+};
 
-export default function Distance({leg}: Props) {
-    console.log(leg)
+export default function Distance({ leg }: Props) {
+  console.log(leg);
+  if (!leg.distance || !leg.duration) return null;
+
   return (
-    <div>Distance</div>
-  )
+    <div>
+      <p>
+        This hospital is <span className="bold">{leg.distance.text}</span> away from your current
+        location. That would take <span className="bold">{leg.duration.text} each direction</span>
+      </p>
+    </div>
+  );
 }
