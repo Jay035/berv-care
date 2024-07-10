c48ce9e0ab971fd05c836c952e9acea711af3aca
Jay035 <jaydencolins08@gmail.com>
2024-07-10 15:03:59 +0100

    fixes

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -23,15 +23,15 @@ export default function HeroSection({}: Props) {
   return (
     <section className="grid items-center lg:grid-cols-2 gap-[7.25rem] lg:h-[90vh] lg:justify-center lg:mt-0 lg:mb-0 mb-[3.375rem] px-8 sm:px-[6vw]">
       <div className="flex flex-col items-center lg:items-start md:w-[32rem] mx-auto lg:w-full">
-        <p className="hero_text text-center lg:text-left font-semibold text-base xl:text-lg text-[#6B7280] mb-1">
+        <p className="hero_text overflow-hidden text-center lg:text-left font-semibold text-base xl:text-lg text-[#6B7280] mb-1">
           Find the Care You Deserve, With Ease
         </p>
-        <h1 className="hero_text font-extrabold text-center lg:text-left text-[#14532D] text-3xl md:text-4xl xl:text-6xl">
+        <h1 className="hero_text font-extrabold overflow-hidden text-center lg:text-left text-[#14532D] text-3xl md:text-4xl xl:text-6xl">
           Your Pathway to Trusted Care Providers
         </h1>
         <p
           data-testid="hero-subtext"
-          className="hero_text text-center lg:text-left mt-4 text-lg xl:text-xl text-[#374151]"
+          className="hero_text text-center overflow-hidden lg:text-left mt-4 text-lg xl:text-xl text-[#374151]"
         >
           With <span className="font-bold text-[#14532D]">Berv-Care</span>,
           finding the nearest care has never been easier. Start your journey to
