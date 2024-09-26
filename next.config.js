bea69947225b8b1ea38e6e68942fcd7275a3d187
Jay035 <jaydencolins08@gmail.com>
2024-09-26 12:03:21 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   env: {
-    rapidApiKey: 'ee5c526905msh02438072c669a06p1aa2bfjsndf913359a642',
-  },
   images: {
     remotePatterns: [
       {
